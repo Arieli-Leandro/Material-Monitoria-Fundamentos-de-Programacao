2506acf2d0f19d726b077232a0e210123b9e7171
Arieli Leandro Gutierres <arieli.leandrogutierres2005@gmail.com>
2026-09-07 15:25:21 -0300

    Add files via upload

---
+++ Exercicios Listas/Modulo 1/exiba_seu_nome.c
@@ -0,0 +1,21 @@
+/* (Pág.25) Digite o código-fonte do programa para que ele imprima o seu nome na tela. */
+
+// Ola, eu sou um comentario de uma linha só :D
+
+/* 
+
+    Ola, eu sou um comentario de várias linhas :D 
+
+*/
+
+#include <stdio.h> // Inclui a biblioteca padrao de entrada e saída
+
+// Função principal onde o programa inicia
+int main(){ // Abre o bloco de código da função main
+
+    printf("Ola, meu nome e (seu nome)"); // Imprime o nome na tela 
+
+    return 0; // Retorna 0 informando que o programa finalizou com sucesso
+
+}// Fecha o bloco de código da função main
+
